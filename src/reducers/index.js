eb80571a85be00027337694fdccc01c9f8f8fa17
TB831 <druu831@gmail.com>
2018-07-23 15:08:25 -0700

    wired up reducerBooks.js to index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
+import BooksReducers from './reducerBooks.js';
 
 const rootReducer = combineReducers({
-  
+  books: BooksReducers
 });
 
 export default rootReducer;
