7da0d405afe52e4bc06f77bf3071759e4d21bc3c
TB831 <druu831@gmail.com>
2018-07-23 18:38:02 -0700

    action function selectBook created, wired to BookList container

---
+++ src/actions/index.js
@@ -0,0 +1,4 @@
+// Contains all the different action creaters
+export const selectBook = (book) => {
+  console.log(`A book has been selected: ${book.title}`);
+}
--- src/containers/BookList.js
+++ src/containers/BookList.js
@@ -1,6 +1,8 @@
 // Renders a list of books
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { selectBook } from '../actions/index.js';
+import { bindActionCreaters } from 'redux';
 
 class BookList extends Component {
   renderList() {
@@ -23,4 +25,9 @@ const mapStateToProps = (state) => { // Takes are application state as an argume
   };
 }
 
-export default connect(mapStateToProps)(BookList);  // Returns a smart component(BookList)/ Container
+// Anything returned from this function will end up as props on the BookList container
+const mapDispactToProps = (dispatch) => { // Whenever select book is called, the result should be passed to all our reducers
+  return bindActionCreaters({selectBook: selectBook}, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispactToProps)(BookList);  // Returns a smart component(BookList)/ Container
