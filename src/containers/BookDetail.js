b07e118d79140d713a05cec78ed265b8c8969937
TB831 <druu831@gmail.com>
2018-07-23 19:23:41 -0700

    BookDetail container wired up to app.js

--- src/containers/BookDetail.js
+++ src/containers/BookDetail.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react'
 export default class BookDetail extends Component {
   render() {
     return (
-      
-    )
+      <div>
+        Book Detail!
+      </div>
+    );
   }
 }
