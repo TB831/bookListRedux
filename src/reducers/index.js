918ec77fe6bfe2892d1ab600e975f4982ad90074
TB831 <druu831@gmail.com>
2018-07-23 19:21:38 -0700

    BookDetail container setup

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import BooksReducers from './reducerBooks.js';
+import ActiveBook from './reducerActiveBook.js';
 
 const rootReducer = combineReducers({
-  books: BooksReducers
+  books: BooksReducers,
+  activeBook: ActiveBook
 });
 
 export default rootReducer;
