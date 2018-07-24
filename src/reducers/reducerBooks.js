3be1071168298e3e539caa01b768cde7dadd8cb0
TB831 <druu831@gmail.com>
2018-07-23 19:39:15 -0700

    added pages detail

--- src/reducers/reducerBooks.js
+++ src/reducers/reducerBooks.js
@@ -1,8 +1,8 @@
 export default function() {
   return [
-    {title: 'JavaScript: The Good Parts'},
-    {title: 'Harry Potter'},
-    {title: 'The Dark Tower'},
-    {title: 'Eloquent Ruby'}
+    {title: 'JavaScript: The Good Parts', pages: 101},
+    {title: 'Harry Potter', pages: 932},
+    {title: 'The Dark Tower', pages: 231},
+    {title: 'Eloquent Ruby', pages:823}
   ]
 }
